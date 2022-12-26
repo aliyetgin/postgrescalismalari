f118aa7eecd63e04fdf613888a0dd190436d0be8
aliyetgin <yetgin.ali@outlook.com>
2022-12-26 14:06:27 +0300

    Update postgrescalismalari.sql
    İkinci ödev

--- postgrescalismalari.sql
+++ postgrescalismalari.sql
@@ -1,5 +1,4 @@
 -- İLK ÖDEV
-
 -- SELECT title, description FROM film;
 
 --SELECT * FROM film
@@ -13,4 +12,16 @@
 
 --SELECT * FROM film
 --WHERE length <= 50 AND (rental_rate != 2.99 AND rental_rate <> 4.99);
+
+
+--İKİNCİ ÖDEV
+--SELECT * FROM film
+--WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+
+--SELECT first_name, last_name FROM actor
+--WHERE first_name IN ('Penelope', 'Nick', 'Ed');
+
+--SELECT * FROM film
+--WHERE (rental_rate IN (0.99, 2.99, 4.99)) AND (replacement_cost IN (12.99, 15.99, 28.99));
+
 
