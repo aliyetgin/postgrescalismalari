7a324dc907eef72fee06799b63ab46ea360a737d
aliyetgin <yetgin.ali@outlook.com>
2023-02-14 19:13:26 +0300

    Update postgrescalismalari.sql
    Altıncı ödev

--- postgrescalismalari.sql
+++ postgrescalismalari.sql
@@ -69,6 +69,20 @@ WHERE store_id = 1
 ORDER BY last_name DESC
 LIMIT 4; */
 
+ALTINCI ÖDEV
+--SELECT ROUND(AVG(rental_rate), 2) FROM film;
+
+/* SELECT COUNT(*) FROM film
+WHERE title LIKE 'C%'; */
+
+/* SELECT MAX(length) FROM film
+WHERE rental_rate = 0.99; */
+
+/* SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 150; */
+
+
+
 
 
 
